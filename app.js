7a3503aa7de0ea47f51787dbe7a45b65732e4ab8
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-21 16:04:57 +0700

    js lab 126

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+// ให้สร้าง array result ให้ได้ผลลัพธ์ตามเงื่อนไขที่กำหนด โดยใช้ arr.filter
+
+const array = [
+  { name: "Ben", age: 14 },
+  { name: "Phil", age: 18 },
+  { name: "John", age: 32 },
+  { name: "Ann", age: 16 },
+  { name: "Paul", age: 24 },
+];
+
+const array2 = array.filter((arr) => {
+  if (arr.age >= 18) {
+    return arr;
+  }
+});
+console.log(array2)
+// อายุไม่น้อยกว่า 18
